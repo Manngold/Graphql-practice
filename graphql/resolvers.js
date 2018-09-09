8a6d2963212b51099bdc989194850fd3668d6dd1
manngold <jsw4820@gmail.com>
2018-09-09 22:33:08 +0900

    final touch

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,10 @@
-import { getMovies } from "./db";
+import { getMovies, getSuggestions } from "./db";
 
 const resolvers = {
     Query: {
         movies: (_, {limit, rating}) => getMovies(limit, rating),
+        movie: (_, { id }) => getMovies(id),
+        suggestion: (_, { id }) => getSuggestions(id)
     }
 };
 
